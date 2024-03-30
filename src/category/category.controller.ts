4a92a483d38c73e1b16962c04cfe42f7fc6d0edb
Rodrigo Lapertosa <rodlapertosa@gmail.com>
2024-03-29 21:05:07 -0300

    feat: config swagger

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { Category } from 'src/domain/entity/category.entity';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('category')
+@ApiTags('Category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'reflect-metadata';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('E-commerce-panel')
+    .setDescription('Base Api')
+    .setVersion('0.1')
+    // .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger-ui', app, document, {});
   await app.listen(3000);
 }
-bootstrap();
+bootstrap().then(() => console.log('Service listening 👍: ', 3000));
