2fdb3c56d7ed52eed7e96b28249f4cdf0c1ae110
Rodrigo Lapertosa <rodlapertosa@gmail.com>
2024-09-03 14:49:35 -0300

    test: create getCategoryById test

---
+++ src/useCase/category/getCategoryById.use-case.spec.ts
@@ -0,0 +1,34 @@
+import { Category } from 'src/domain/entity/category.entity';
+import { GetCategoriesUseCase } from './getCategories.use-case';
+
+describe('Get categories Use Case', () => {
+  const mockRepositoryReturn: Category[] = [
+    {
+      id: 'abc',
+      name: 'teste',
+    },
+    {
+      id: 'cba',
+      name: 'teste2',
+    },
+  ];
+
+  const CategoryRepositoryStub = jest.fn().mockImplementation(() => ({
+    findAll: jest.fn(),
+  }));
+
+  it('success list cycle', async () => {
+    const categoryRepositoryStub = new CategoryRepositoryStub();
+    const getCategoriesUseCase = new GetCategoriesUseCase(
+      categoryRepositoryStub,
+    );
+
+    jest
+      .spyOn(categoryRepositoryStub, 'findAll')
+      .mockResolvedValueOnce(mockRepositoryReturn);
+
+    const data = await getCategoriesUseCase.execute();
+
+    expect(data).toEqual(mockRepositoryReturn);
+  });
+});
